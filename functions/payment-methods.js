203c8aa62323805c75e999c6e8fe75c9fa025cdf
adrbosch <65780232+adrbosch@users.noreply.github.com>
2020-06-25 10:45:44 -0500

    Add files via upload

--- functions/payment-methods.js
+++ functions/payment-methods.js
@@ -11,13 +11,13 @@ exports.handler = async function(event, context, callback) {
 
     // Validate that the request is coming from Snipcart
     // const response = await fetch(`https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${request.PublicToken}`)
-    const response = await fetch(`https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${cartRequest.PublicToken}`)
+    // const response = await fetch(`https://payment.snipcart.com/api/public/custom-payment-gateway/validate?publicToken=${cartRequest.PublicToken}`)
 
-    // Return a 404 if the request is not from Snipcart
-    if (!response.ok) return {
-        statusCode: 404,
-        body: ""
-    }
+    // // Return a 404 if the request is not from Snipcart
+    // if (!response.ok) return {
+    //     statusCode: 404,
+    //     body: ""
+    // }
 
     // Create a payment method list
     let paymentMethodList = [{
