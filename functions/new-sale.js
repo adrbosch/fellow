c01e97b59036660f83f894895d0393c4951f6816
adrbosch <65780232+adrbosch@users.noreply.github.com>
2020-10-11 19:19:03 -0500

    Add files via upload

--- functions/new-sale.js
+++ functions/new-sale.js
@@ -11,7 +11,8 @@ exports.handler = async (event, context) => {
   
     // When the method is POST, the name will no longer be in the event’s
     // queryStringParameters – it’ll be in the event body encoded as a query string
-    const params = querystring.parse(event.body);
+    //const params = querystring.parse(event.body);
+    const params = JSON.parse(event.body);
     const orderCompleted = params.eventName || "nope";
 
     if (orderCompleted == "order.completed") {
