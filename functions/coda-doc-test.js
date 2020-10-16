d4f10412e484bab0e772de4911c92b13209a07e6
adrbosch <65780232+adrbosch@users.noreply.github.com>
2020-10-16 15:08:28 -0500

    Add files via upload

--- functions/coda-doc-test.js
+++ functions/coda-doc-test.js
@@ -5,7 +5,7 @@ const coda = require('coda-js')
 const codajs = new Coda(process.env.GATSBY_CODA_AUTH);
 
 exports.handler = async function(event, context, callback) {
-    const docs = await codajs.listDocs();
+    const docs = await codajs.getDoc(process.env.GATSBY_CODA_DOC);
     return {
         statusCode: 200,
         body: JSON.stringify({
