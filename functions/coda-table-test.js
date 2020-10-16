3cdec2fe930c931834a6ee397b9eb9af47e92d15
adrbosch <65780232+adrbosch@users.noreply.github.com>
2020-10-16 15:22:49 -0500

    Add files via upload

--- functions/coda-table-test.js
+++ functions/coda-table-test.js
@@ -5,8 +5,8 @@ const coda = require('coda-js')
 const codajs = new Coda(process.env.GATSBY_CODA_AUTH);
 
 exports.handler = async function(event, context, callback) {
-    const docs = await codajs.listDocs();
-    const table = docs.getTable(process.env.GATSBY_CODA_TABLE);
+    const table = await codajs.getTable(process.env.GATSBY_CODA_DOC, process.env.GATSBY_CODA_TABLE);
+
     return {
         statusCode: 200,
         body: JSON.stringify({
